7bdebcad0dd22dd5920509830a3764bb8fb5a23b
Arif2ankhi <arif2ankhi@gmail.com>
2024-12-16 06:42:19 +0000

    Addes jwt token

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -7,8 +9,13 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 
-app.use( cors());
+app.use( cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 // DB_USER = job_hunter
 // DB_PASS = 0nKYr2xcZe0L4i9a
@@ -39,6 +46,30 @@ async function run() {
 
     const jobsCollection = client.db('jobPortal').collection('jobs');
     const jobApplicationCollection = client.db('jobPortal').collection('job_applications');
+
+
+    // Auth related APIs
+
+    app.post('/jwt', async (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.JWT_SECRET,  {expiresIn: '1hr'});
+        console.log(token)
+
+
+
+        res
+        .cookie('token', token, {
+            httpOnly: true,
+            secure:false,
+            sameSite: 'strict'
+            // http://localhost:5173/signIn
+
+
+        })
+        .send({ success: true });
+
+
+    })
 
 
     // Jobs related Apis 
@@ -76,6 +107,9 @@ async function run() {
     app.get('/job-application', async (req, res) => {
         const email = req.query.email;
         const query = {applicant_email: email}
+
+        console.log('Cuk cuk cookies', req.cookies);
+
         const result = await jobApplicationCollection.find(query).toArray();
 
         //  not the best way, fokira way as per PH Team 
@@ -140,8 +174,6 @@ async function run() {
       res.send(result);
 
     })
-
-
 
 
     app.patch('/job-applications/:id', async (req, res) => {
